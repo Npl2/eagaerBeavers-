e69c2a704a699340de1965c0f6c500ae5ee50161
Harsh <hp548@njit.edu>
2024-03-04 13:14:12 -0500

    added some queries for MongoDB

--- mongoClient.php
+++ mongoClient.php
@@ -11,14 +11,36 @@ class MongoClientDB {
         $connectionString = "mongodb+srv://hp548:Winter2022@systemintg.aahdnzc.mongodb.net/?retryWrites=true&w=majority&appName=SystemIntg";
         $apiVersion = new ServerApi(ServerApi::V1);
         $this->client = new MongoDB\Client($connectionString, [], ['serverApi' => $apiVersion]);
-        $this->db = $this->client->selectDatabase('cardeal');
+        $this->db = $this->client->selectDatabase('carDeal');
     }
 
+    // find user
     public function findUserByUsername($username) {
         $collection = $this->db->selectCollection('users');
         return $collection->findOne(['username' => $username]);
     }
 
+    // inserting user
+    public function insertUser($username, $password) {
+      $collection = $this->db->selectCollection('users');
+      $existingUser = $collection->findOne(['username' => $username]);
+      if ($existingUser) {
+          return ['success' => false, 'message' => 'User already exists.'];
+      }
+      
+      $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+      $result = $collection->insertOne([
+          'username' => $username,
+          'password' => $hashedPassword,
+      ]);
+      
+      if ($result->getInsertedCount() == 1) {
+          return ['success' => true, 'message' => 'User successfully inserted.'];
+      } else {
+          return ['success' => false, 'message' => 'Failed to insert user.'];
+      }
+    }
+  
     // checking
     public function isDatabaseConnected() {
         try {
@@ -28,5 +50,6 @@ class MongoClientDB {
             return false;
         }
     }
+
 }
 ?>
