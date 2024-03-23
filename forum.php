a404021f750d69b49d1955ed0953e54f42bfdbae
awuraadjei <aaa373@njit.edu>
2024-03-22 23:25:40 -0400

    made changes to forum.php

--- forum.php
+++ forum.php
@@ -35,8 +35,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Discussion Forum</title>
-        <link href="css/forum.css" rel="stylesheet"> 
+        <!-- <link href="css/forum.css" rel="stylesheet">  -->
         <link href="css/header.css" rel="stylesheet">
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body>
         <?php include 'header.php'; ?>
