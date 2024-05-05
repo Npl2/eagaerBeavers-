5db3ccf488eb61dacf3f595f38d437d5666755ae
npl2 <npl2@njit.edu>
2024-05-05 18:14:28 -0400

    Added copies of conf files for DMZQA

---
+++ DMZConfiguration/rsyncLogs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sync_logs() {
+    local ssh_command="$1"
+    local source_file="$2"
+    local destination="$3"
+
+    rsync -avz -e "$ssh_command" "$source_file" "$destination"
+}
+
+ssh_command="ssh -i /home/dmz/.ssh/dmzErrorLog"
+
+source_file="/var/log/syslog"
+
+destination1="npl2@backendQA:/home/npl2/errorLogs/dmzErrorLogs.log"
+destination2="webserver@frontendQA:/home/webserver/errorLogs/dmzErrorLogs.log"
+
+while true; do
+    sync_logs "$ssh_command" "$source_file" "$destination1"
+    sync_logs "$ssh_command" "$source_file" "$destination2"
+    sleep 30
+done
+
